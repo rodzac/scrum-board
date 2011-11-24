bb03043baa7e36582735c52095a8ea5dd00a2e72
ROd <rodzac@gmail.com>
2011-11-23 23:22:04 -0200

    first commit

---
+++ src/main/java/org/github/rzdrigo/scrumboard/Start.java
@@ -0,0 +1,21 @@
+package org.github.rzdrigo.scrumboard;
+
+import static org.webbitserver.WebServers.createWebServer;
+
+import java.io.IOException;
+
+import org.webbitserver.WebServer;
+import org.webbitserver.handler.exceptions.PrintStackTraceExceptionHandler;
+
+public class Start
+{
+	public static void main(final String[] args) throws IOException
+	{
+        final WebServer webServer = createWebServer(Integer.parseInt(args[0]));
+        webServer.add("/ws",new BoardWebSocketHanlder());
+        webServer.add(new FileHttpHandler());
+        webServer.connectionExceptionHandler(new PrintStackTraceExceptionHandler()).start();
+
+        System.out.println("running on: " + webServer.getUri());
+	}
+}
